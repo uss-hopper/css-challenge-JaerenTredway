7fb5c93a9ca1ab42a6d119daba8661f771202d5a
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-31 14:49:03 -0700

    Made changes to css.

--- index.php
+++ index.php
@@ -12,20 +12,21 @@
 	</head>
 
 	<body>
-		<div>
-
 			<!-- HEADER -->
 			<header>
+				<div class="container">
 					<h1>Dylan McDonald:~$</h1>
-					<em>Computer Scientist. Cat Lover. Picard Fan.</em>
+					<em>Compupu Scientist. Cat Lover. Picard Fan.</em>
+				</div>
 			</header>
 
 			<!-- INSTRUCTIONS PANEL -->
-			<section>
+			<section class="instructions">
 				<div class="container">
 
-					<!-- WELCOME IMAGE -->
-					<img src="images/welcome.gif" alt="welcome to my homepage">
+					<div class="image-container">
+						<img class="image" src="images/welcome.gif" alt="welcome to my homepage">
+					</div>
 
 					<div class="instructions-wrapper">
 						<div class="instructions-content">
@@ -37,7 +38,7 @@
 								<li>Have fun! The solution will be demonstrated in class. <small>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</small></li>
 							</ol>
 						</div>
-						<img src="images/captain-janeway.png" alt="captain janeway" />
+						<img class="image" src="images/captain-janeway.png" alt="captain janeway" />
 						<div class="clearfix"></div>
 					</div><!--/.instructions-->
 				</div><!--/.container-->
@@ -46,7 +47,7 @@
 			<!-- ABOUT ME -->
 			<section class="about-me">
 				<div class="container">
-					<img src="images/stunning.jpg" alt="captain picard" />
+					<img class="image" src="images/stunning.jpg" alt="captain picard" />
 					<h2>A Day in the Life...</h2>
 					<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
 					<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
@@ -115,8 +116,6 @@
 					</div>
 				</div>
 			</section>
-
-		</div><!--/.sfooter-content-->
 
 		<footer>
 			<div class="container">
